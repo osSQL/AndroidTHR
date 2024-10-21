f58e82811479ea25455e349f97b5dff92d536038
Cybersport <Cybersport@26kadr.internal>
2024-10-21 13:34:33 +0300

    0.1

---
+++ app/src/main/java/com/example/pols5/about.java
@@ -0,0 +1,30 @@
+package com.example.pols5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class about extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_about);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void Nav3(View view){
+        Intent intent = new Intent(about.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
